a9ada843bf0a3436fb636a4cd5b17ba35f44b9b7
Emilia <143875546+Emilka56@users.noreply.github.com>
2024-03-30 00:18:53 +0300

    Яхудина Эмилия 11-305

--- HashMap.java
+++ HashMap.java
@@ -1,149 +1,116 @@
-import java.util.ArrayList;
-public class HashMap<K, V> implements Map<K, V> {
-
-    public class Node {  //узлы массива
-        public int hash;
-        public K key;
-        public V value;
-        Node next;
-        Node prev;
-
-        Node (K key, V value) { //конструктор узлов
+public class HashMap<K,V> {
+    private Node <K,V> table [];
+    static final int DEFAULT_TABLE_CAPACITY = 16;
+    class Node<K,V>{
+        final int hash;
+        final K key;
+         V value;
+        Node <K, V> next;
+        Node <K, V> prev;
+        Node(K key, V value) {
+            this.hash = hashCode();
             this.key = key;
             this.value = value;
-            this.hash = this.hashCode();
+        }
+        public K getKey(){
+            return this.key;
+        }
+        public V getValue(){
+            return this.value;
+        }
+        public void setValue(V value){ this.value=value;}
+    }
+
+    HashMap(){
+        table = new Node[DEFAULT_TABLE_CAPACITY];
+    }
+
+    public void put(K key, V value){ // добавление обьекта
+        int hash = key.hashCode() % DEFAULT_TABLE_CAPACITY; //высчитываем хэш
+        Node <K,V> dvizhok = table[hash];
+
+        if (dvizhok==null){
+            table[hash] = new Node<K, V>(key, value);
+        } else {
+            while(dvizhok.next!=null){
+                if (dvizhok.getKey()==key){
+                    dvizhok.setValue(value); //если ключ совпадет меняем значение
+                    return;
+                }
+                dvizhok=dvizhok.next; //передвигаем движок
+            }
+            if (dvizhok.getKey()==key){ //если добрались до последнего движка
+                dvizhok.setValue(value); //если ключ совпадет меняем значение
+                return;
+            }
+            dvizhok.next=new Node<K,V>(key,value); //если дошли до последнего движка и одинакового не нашлось
         }
     }
 
-    public ArrayList<LinkedNode> array; //массив из листов
-
-    HashMap () {;} //создание пустой коллекции
-
-    @Override
-    public void put(K key, V value) {
-        Node node = new Node(key, value); //создаём узел, который будем добавлять в массив
-        if (!containsKey(key)) { //если такого ключа еще нет в HashMap, можем добавлять элемент
-            int hashcode = hashCode(value);
-            boolean flag = false;
-
-            for (int i = 0; i < this.array.size(); i ++) { //ищем лист с нужным нам хэш-кодом
-                if (this.array.get(i).hash == hashcode ) {
-                    this.array.get(i).addNode(node);
-                    flag = true;
+    public boolean containsValue(V value){ //проверка на содержание данного значения
+        for(int i=0;i<DEFAULT_TABLE_CAPACITY;i++){
+            Node <K,V> dvizhok = table[i];
+            while(dvizhok.next!=null){
+                if(dvizhok.getValue()==value){
+                    return true;
                 }
             }
-            if (!flag) { //если в массиве нет листа с нужным нам хэш-кодом, создаем новый лист в массиве
-                LinkedNode linked_node = new LinkedNode();
-                linked_node.hash = hashcode;
-                int arraySize = this.array.size();
-                this.array.get(arraySize).addNode(node);
-            }
-        } else {
-            System.out.println("Значение с таким ключом уже присутствует в HashMap");
-        }
+        } return false;
     }
 
-    @Override
-    public V get(K key) {
-        Node current = this.array.get(0).tale; //начинаем исктать с первого элемента в нклевом листе
-        int counterOfLists = 0;
-        while (current.key != key) {
-            if (current == this.array.get(counterOfLists).head) { //если мы дошли до головы листа в первом элементе, переопределяем хвост
-                counterOfLists++;
-                current = this.array.get(counterOfLists).tale;
+    public boolean containsKey(K key){//проверка на содержание данного ключа
+        for(int i=0;i<DEFAULT_TABLE_CAPACITY;i++){
+            Node <K,V> dvizhok = table[i];
+            while(dvizhok.next!=null){
+                if(dvizhok.getKey()==key){
+                    return true;
+                }
             }
-            current = current.next;
-        }
-        return current.value;
+        } return false;
     }
 
-    @Override
-    public boolean containsKey(K key) {
-        Node current = this.array.get(0).tale; //начинаем исктать с первого элемента в нклевом листе
-        int counterOfLists = 0;
-        boolean flag = false;
-        for (int i = 0; i < this.size(); i++) {
-            if (current == this.array.get(counterOfLists).head) { //если мы дошли до головы листа в первом элементе, переопределяем хвост
-                counterOfLists++;
-                current = this.array.get(counterOfLists).tale;
-            }
-            if (current.key == key) { //если нашли необходимое значение, выхлдим из цикла
-                flag = true;
-                break;
-            }
-            current = current.next;
+    public V get(K key){ //нахождение значения по ключу обьекта
+        int hash = key.hashCode() % DEFAULT_TABLE_CAPACITY;
+        Node <K,V> dvizhok = table[hash]; //нашли подходящий бакет
+
+        if (dvizhok==null){ //если  бакет пустой
+            return null;
         }
-        return flag;
+        while(dvizhok!=null){
+            if(dvizhok.getKey()==key){
+                return dvizhok.getValue();
+            }
+            dvizhok=dvizhok.next;
+        }
+        return null; //соответствующие значение не найдено
     }
 
-    @Override
-    public boolean containsValue(V value) {
-        Node current = this.array.get(0).tale; //начинаем исктать с первого элемента в нулевом листе
-        int counterOfLists = 0;
-        boolean flag = false;
+    public void remove(K key){ //удаление обьекта по ключу
+        int hash = key.hashCode() % DEFAULT_TABLE_CAPACITY;
+        Node <K,V> dvizhok = table[hash];
 
-        for (int i = 0; i < this.size(); i++) {
-            if (current == this.array.get(counterOfLists).head) { //если мы дошли до головы листа в первом элементе, переопределяем хвост
-                counterOfLists++;
-                current = this.array.get(counterOfLists).tale;
-            }
-            if (current.value == value) { //если нашли необходимое значение, выхлдим из цикла
-                flag = true;
-                break;
-            }
-            current = current.next;
-        }
-        return flag;
-    }
-
-    @Override
-    public void remove(K key) {
-        Node current = this.array.get(0).tale; //начинаем исктать с первого элемента в нклевом листе
-        int counterOfLists = 0;
-        while (current.key != key) {
-            if (current == this.array.get(counterOfLists).head) { //если мы дошли до головы листа в первом элементе, переопределяем хвост
-                counterOfLists++;
-                current = this.array.get(counterOfLists).tale;
-            }
-            current = current.next;
+        if (dvizhok==null){ //если  бакет пустой
+            return;
         }
 
-        (current.prev).next = current.next; //переопределяем указатели последующего и предыдущего элементов
-        (current.next).prev = current.prev;
+        if (dvizhok.getKey()==key){ //если найденный обьект в начале бакета
+            table[hash]=dvizhok.next;
+            dvizhok.next=null;
+            return;
+        }
 
-        current.prev = null;
-        current.next = null;
-    }
-
-    @Override
-    public int size() {
-        int counterOfNodes = 0; //счетчик эдементов коллекции
-        Node current;
-
-        for (int i = 0; i < this.array.size(); i++) { //проходимся по каждому листу
-            current = this.array.get(i).tale; //перед каждым прохождением по листу, переопределяем текущий элемент
-            while (current != this.array.get(i).head) { //пока не дойдем до головы списка
-                counterOfNodes ++;
-                current = current.next;
+        while(dvizhok !=  null){ //если найденный обьект в середине бакета
+            if(dvizhok.getKey()==key){
+                dvizhok.prev=dvizhok.next;
+                dvizhok.next=null;
+                return;
             }
+            dvizhok.prev=dvizhok;
+            dvizhok=dvizhok.next;
         }
-        return counterOfNodes;
+        return; //если такого обекта нет
     }
 
-    @Override
-    public int hashCode(V value) {
-        // Преобразование объекта в строку
-        String str = value.toString();
-        int sum = 0;
-
-        for (int i = 0; i < str.length(); i++) {
-            int ascii = (int) str.charAt(i); //получение кода ASCII символа
-            sum += ascii;
-        }
-        return sum;
-    }
-
-    @Override
     public boolean equals(K key1, K key2) {
         if (get(key1) == get(key2)) {
             return true;
